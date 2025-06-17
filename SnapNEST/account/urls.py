ca8fc3e5338e6530e3b6222d8c56bb03aeecec60
Iam-samyog <msamyog37@gmail.com>
2025-06-17 21:59:12 +0545

    Added registration.html

--- SnapNEST/account/urls.py
+++ SnapNEST/account/urls.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns=[
+    #previous Login url
+    #path ('Login',views.user_Login,name='login),
+    #Login /Logout urls
+    
     path('login/',views.user_login,name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 ]
