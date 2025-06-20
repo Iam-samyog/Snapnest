8f8751611cc7efb25136d20ecb41771653b11cc1
Iam-samyog <msamyog37@gmail.com>
2025-06-20 20:00:38 +0545

    Final commit

--- SnapNEST/SnapNEST/settings.py
+++ SnapNEST/SnapNEST/settings.py
@@ -153,3 +153,14 @@ MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 
 
+SOCIAL_AUTH_PIPELINE = [
+ 'social_core.pipeline.social_auth.social_details',
+ 'social_core.pipeline.social_auth.social_uid',
+ 'social_core.pipeline.social_auth.auth_allowed',
+ 'social_core.pipeline.social_auth.social_user',
+ 'social_core.pipeline.user.get_username',
+ 'social_core.pipeline.user.create_user',
+ 'account.authentication.create_profile',
+ 'social_core.pipeline.social_auth.associate_user',    'social_core.pipeline.social_auth.load_extra_data', 
+ 'social_core.pipeline.user.user_details',
+]
