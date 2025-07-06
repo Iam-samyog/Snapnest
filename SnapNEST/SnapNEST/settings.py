dd4c13edfd6734e30721ffff00a120446cae3e23
Iam-samyog <msamyog37@gmail.com>
2025-07-06 18:49:57 +0545

    Added views using redis

--- SnapNEST/SnapNEST/settings.py
+++ SnapNEST/SnapNEST/settings.py
@@ -177,6 +177,11 @@ ABSOLUTE_URL_OVERRIDES={
     'auth.user':lambda u:reverse_lazy('user_detail',args=[u.username])
 }
 
+#Redis server
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
 
 INTERNAL_IPS=[
     '127.0.0.1',
--- SnapNEST/images/views.py
+++ SnapNEST/images/views.py
@@ -9,7 +9,15 @@ from django.views.decorators.http import require_POST
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from django.http import HttpResponse
 from actions.utils import create_action
+import redis
+from django.conf import settings
 
+#connect to redis
+r=redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB
+)
 
 @login_required
 def image_create(request):
@@ -39,10 +47,17 @@ def image_create(request):
 
 def image_detail(request,id,slug):
     image=get_object_or_404(Image,id=id,slug=slug)
+    #increment total image views by 1
+    total_views=r.incr(f'image:{image.id}:views')
     return render(
         request,
         'images/image/detail.html',
-        {'section':'images','image':image} 
+        {
+            'section':'images',
+            'image':image,
+            'total_views':total_views
+         
+         } 
     )
  
 
@@ -92,3 +107,4 @@ def image_list(request):
         'images/image/list.html',
         {'section':'images','images':images}
     )
+    
